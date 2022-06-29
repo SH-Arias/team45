d0104e8736aba42a6534982f1494d46c3fa8e11d
santiago pachon <santiago@ubidots.com>
2022-06-28 22:44:11 -0500

    initial commit

---
+++ pages/home.py
@@ -0,0 +1,23 @@
+import dash
+from dash import html , dcc
+import pandas as pd
+import dash_bootstrap_components as dbc
+from dash_labs.plugins import register_page
+
+register_page(__name__, path="/")
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                html.H2('CHARACTERIZATION AND CLASSIFICATION OF LIDAR SENSOR DATA',style={'marginTop':'4rem','textAlign':'center','color':'#50006e','marginBottom':'5rem'}),
+                dbc.Col(
+                    html.Div(html.Img(src=app.get_asset_url('imageedit_1_6955781055.png'), style={'width':'70%', 'textAlign':'center'})),
+                    width={"size": 7, "offset": 4},
+                    ) 
+            ]
+        )
+    ]
+)
