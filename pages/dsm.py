eab9900cc8b7724670275825c84b3248bd560623
santiago pachon <santiago@ubidots.com>
2022-07-07 10:52:18 -0500

    model page added, dataframes updated

--- pages/dsm.py
+++ pages/dsm.py
@@ -6,6 +6,17 @@ from dash_labs.plugins import register_page
 from app import list_of_images
 
 register_page(__name__, path="/dsm")
+data = {'Code': [300, 200, 601], 
+    'X': [5078011.1380, 5078011.5880, 5078078.5400],
+    'Y': [2342928.8960, 2342930.8270, 2342905.5430],
+    'Z': [1343.8460, 1341.7580, 1292.7640]
+}
+
+df = pd.DataFrame.from_dict(data)
+table = dbc.Table.from_dataframe(
+    df, striped=True, bordered=True, hover=True, index=False
+)
+
 
 layout = dbc.Container(
     html.Div(
@@ -27,8 +38,16 @@ layout = dbc.Container(
                         ),
                     dbc.Col(html.Div(
                             [
-                            html.P('Data set are shwon as renderd using the Open3D Python library'),
-                            html.P('Data set are shwon as renderd using the Open3D Python library')
+                            html.H3('DIGITAL SURFACE MODELS'),
+                            html.Br(),
+                            html.P('Heres how raw data looks like:'),
+                            table,
+                            html.P('There are 11 data-sets representing equal number of sections \
+                                of the Toledo-Zamore transmmision line. \
+                                This data-sets are shown are displayed as \
+                                rendered using the Open3D Python library.\
+                                '),
+                            html.P('The colors vary from blue to red on the images according to the X coordinate, being blue the lowest and red the higher position along that axis.')
                             ]
                         ),
                         width={'size':4}
--- pages/model.py
+++ pages/model.py
@@ -4,19 +4,32 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 from dash_labs.plugins import register_page
 
+register_page(__name__, path="/model")
+
+lista = ["K-Nearest Neighbour","Decision Tree Classifier","Random random forest","Extra Tree Classifier"]
+
 layout = dbc.Container(
     [
         dbc.Row(
             [
-            dbc.Col(
-                html.Div(
-                        [
-                            html.H4("MDS-01",style={'marginTop':'4rem','textAlign':'center'}),
-                            html.H4("MDS-02",style={'textAlign':'center'})
-                            
-                        ]
-                    )                                                                                  
-                )
+        
+            html.Div(
+                    [
+                        html.H4("Models for classification",style={'marginTop':'4rem','textAlign':'center'}),
+                        html.Br(),
+                        html.P("In order to accoplish the objective of creating a model to clasify lidar data based on\
+                            data already classified, the following models were tested:"),
+                        html.Ol([html.Li(x) for x in lista]),
+                        html.P("We obtained the best results using the extra tree classifier\
+                            but in no case more than 50 percent accuaracy, this means the model assigned \
+                            most of the time code 300 for less than 1 meter high tree, and natural terrain. This made \
+                                necessary the implementation of a data aumentation algorithm "),
+                        html.Br(),
+                        html.P()
+                        
+                    ]
+                )                                                                                  
+                
             ]
         )
     ]
