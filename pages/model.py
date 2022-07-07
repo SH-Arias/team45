3793d4f40faf5ca72f0cc02f144dd53e9243bb8f
santiago pachon <santiago@ubidots.com>
2022-07-07 12:08:33 -0500

    model and results added

--- pages/model.py
+++ pages/model.py
@@ -7,6 +7,13 @@ from dash_labs.plugins import register_page
 register_page(__name__, path="/model")
 
 lista = ["K-Nearest Neighbour","Decision Tree Classifier","Random random forest","Extra Tree Classifier"]
+lista2 = ["Random Sample","Cluster Sample","Displacement"]
+data = {'Code': [300, 200, 601], 
+    'X': [5078011.1380, 5078011.5880, 5078078.5400],
+    'Y': [2342928.8960, 2342930.8270, 2342905.5430],
+    'Z': [1343.8460, 1341.7580, 1292.7640]
+}
+
 
 layout = dbc.Container(
     [
@@ -25,8 +32,11 @@ layout = dbc.Container(
                             most of the time code 300 for less than 1 meter high tree, and natural terrain. This made \
                                 necessary the implementation of a data aumentation algorithm "),
                         html.Br(),
-                        html.P()
-                        
+                        html.P("We tested various techniques for that process including:"),
+                        html.Ol([html.Li(x) for x in lista2]),
+                        html.Br(),
+                        html.H4("Results using extra tree classifier"),
+                        html.Div(html.Img(src=('https://storage.googleapis.com/lidar-data-01/images/classification.png'), style={'width':'70%', 'textAlign':'center'}))
                     ]
                 )                                                                                  
                 
